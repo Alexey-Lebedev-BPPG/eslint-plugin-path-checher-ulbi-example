220e1ae9f40e21c3c9e1605caf3cc3c2be1021dd
alexey <a.lebedev.bppg@gmail.com>
2023-04-10 23:00:46 +0400

    add aliases

--- tests/lib/rules/path-checker.js
+++ tests/lib/rules/path-checker.js
@@ -11,21 +11,58 @@
 const rule = require("../../../lib/rules/path-checker"),
   RuleTester = require("eslint").RuleTester;
 
-
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
+// тестирование пакета
+const ruleTester = new RuleTester({
+  // добавляем поддержку импортов
+  parserOptions: { ecmaVersion: 6, sourceType: "module" },
+});
 ruleTester.run("path-checker", rule, {
   valid: [
-    // give me some code that won't trigger a warning
-  ],
-
-  invalid: [
     {
-      code: "test error",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      // где расположен файл
+      filename:
+        "/home/alexey/Рабочий стол/Alexey/Ulbi-Example/src/entities/Article",
+      // какую строчку тестим
+      code: "import { addCommentFormActions, addCommentFormReducer, } from '../../model/slice/addCommentForm';",
+      // какой результат ошибки (ввиду того, что тестим положительный кейс, здесь их не должно быть)
+      errors: [],
+    },
+  ],
+  // тестирование неправильного импорта
+  invalid: [
+    // тестируем без алиаса
+    {
+      // где расположен файл
+      filename:
+        "/home/alexey/Рабочий стол/Alexey/Ulbi-Example/src/entities/Article",
+      // какую строчку тестим
+      code: "import { addCommentFormActions, addCommentFormReducer, } from 'entities/Article/model/slice/addCommentForm';",
+      // какой результат ошибки
+      errors: [
+        {
+          message: "В рамках одного слайса все пути должны быть относительными",
+        },
+      ],
+    },
+    // тестируем с алиасом
+    {
+      // где расположен файл
+      filename:
+        "/home/alexey/Рабочий стол/Alexey/Ulbi-Example/src/entities/Article",
+      // какую строчку тестим
+      code: "import { addCommentFormActions, addCommentFormReducer, } from '@/entities/Article/model/slice/addCommentForm';",
+      // какой результат ошибки
+      errors: [
+        {
+          message: "В рамках одного слайса все пути должны быть относительными",
+        },
+      ],
+      // передаем алиас в плагин
+      options: [{ alias: "@" }],
     },
   ],
 });
